a4678e696bcfc0c6572d9aa06c880aff39c066fc
jparsons153 <jparsons153@hotmail.com>
2022-09-21 15:01:34 +0100

    added Eureka server and client settings

---
+++ src/test/java/com/codingnomads/spring/managementmicroservice/ManagementMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingnomads.spring.managementmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
